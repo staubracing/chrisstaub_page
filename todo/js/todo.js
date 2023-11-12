7b2c4a10aa2ac7cbac395108a9bed57f3cb115c2
StaubRacing <staubracing@gmail.com>
2023-11-12 11:06:07 -0600

    Updated: Radio Buttons

--- todo/js/todo.js
+++ todo/js/todo.js
@@ -1,41 +1,4 @@
-function saveTasks() {
-  let tasks = [];
-  document.querySelectorAll('#taskList li').forEach(li => {
-    // Assuming the task text is the node following the radio button
-    let taskText = li.childNodes[1].nodeValue.trim();
-    tasks.push(taskText);
-  });
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-}
-
-function loadTasks() {
-  let tasks = JSON.parse(localStorage.getItem('tasks'));
-  if (tasks) {
-    tasks.forEach(task => {
-      addTask(task);
-    });
-  }
-}
-
-window.onload = loadTasks;
-
-function clearAllTasks() {
-  // Clear the tasks from the DOM
-  document.getElementById('taskList').innerHTML = '';
-
-  // Clear the tasks from localStorage
-  localStorage.removeItem('tasks');
-}
-
-function clearSelectedTasks() {
-  let tasks = document.querySelectorAll('#taskList li input[type="radio"]:checked');
-
-  tasks.forEach(task => {
-    task.parentElement.remove(); // Removes the list item
-  });
-
-  saveTasks(); // Update the tasks in localStorage after deletion
-}
+// script for loading the task list from localStorage
 
 function addTask(taskValue) {
   let task = taskValue || document.getElementById('newTask').value;
@@ -68,4 +31,48 @@ function addTask(taskValue) {
     alert('Please enter a task.');
   }
 }
+
+/**
+ * Add a task to the task list
+ * @param {string} taskValue - The task to add to the task list with the given value or empty string
+ * */
+
+function saveTasks() {
+  let tasks = []; // Create an empty array to store the tasks
+  document.querySelectorAll("#taskList li").forEach((li) => { // Loop through each list item in the task list
+    let taskText = li.childNodes[1].nodeValue.trim(); // Get the text of the task and trim any whitespace from the beginning and end
+    tasks.push(taskText); // Add the task to the array of tasks to save to localStorage
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks)); // Save the tasks to localStorage as a JSON string
+}
+
+function loadTasks() {
+  let tasks = JSON.parse(localStorage.getItem("tasks"));
+  if (tasks) {
+    tasks.forEach((task) => {
+      addTask(task);
+    });
+  }
+}
+
+window.onload = loadTasks;
+
+function clearAllTasks() {
+  // Clear the tasks from the DOM
+  document.getElementById('taskList').innerHTML = '';
+
+  // Clear the tasks from localStorage
+  localStorage.removeItem('tasks');
+}
+
+function clearSelectedTasks() {
+  let tasks = document.querySelectorAll('#taskList li input[type="radio"]:checked');
+
+  tasks.forEach(task => {
+    task.parentElement.remove(); // Removes the list item
+  });
+
+  saveTasks(); // Update the tasks in localStorage after deletion
+}
+
 
