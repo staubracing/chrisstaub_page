899199021527466413df5f12a2c091cafe326615
StaubRacing <staubracing@gmail.com>
2023-11-13 06:03:58 -0600

    Formatting and removed extra comments

--- todo/js/todo.js
+++ todo/js/todo.js
@@ -1,33 +1,28 @@
-// This JavaScript file contains functions for managing a task list. The task list is stored in the browser's local storage.
-
 /**
  * Adds a new task to the task list.
  * The task can be provided as an argument or taken from the HTML input with ID 'newTask'.
  */
 function addTask(taskValue) {
-  // Determine the task value, preferring taskValue if provided and truthy
+  
   let task = taskValue || document.getElementById("newTask").value;
 
-  // Proceed only if the task is not empty
+  
   if (task.trim() !== "") {
     let newTask = document.createElement("li");
     let checkBox = document.createElement("input");
     checkBox.type = "checkBox";
     checkBox.name = "taskItem";
-    checkBox.value = task; // Link checkbox to the task value
+    checkBox.value = task;
 
-    newTask.appendChild(checkBox); // Add checkbox to the list item
+    newTask.appendChild(checkBox);
 
     let textNode = document.createTextNode(task);
-    newTask.appendChild(textNode); // Add task text to the list item
-
-    // Append the new task to the task list in the UI
+    newTask.appendChild(textNode);
+    
     let taskList = document.getElementById("taskList");
     taskList.appendChild(newTask);
-
    
     document.getElementById("newTask").value = "";
-
     
     saveTasks();
 
@@ -42,20 +37,15 @@ function addTask(taskValue) {
  * Each task's text is extracted from the task list and stored.
  */
 function saveTasks() {
-  let tasks = []; // Initialize an array to store task texts
-
-  // Iterate over each task item in the task list
+  let tasks = [];  
   document.querySelectorAll("#taskList li").forEach((li, index) => {
-    let taskText = li.childNodes[1].textContent.trim(); // Extract and trim task text
+    let taskText = li.childNodes[1].textContent.trim();
     let taskObject = {
       index: index,
       text: taskText
-    }; // Create a task object
-
-    tasks.push(taskObject); // Add the task text to the tasks array
+    };
+    tasks.push(taskObject);
   });
-
-  // Convert the tasks array to a JSON string and save it in localStorage
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
@@ -65,12 +55,9 @@ function saveTasks() {
  * Each stored task is added to the task list using the addTask function.
  */
 function loadTasks() {
-  // Retrieve and parse the tasks from localStorage
   let storedTasks = JSON.parse(localStorage.getItem("tasks"));
 
-  // Check if there are any tasks stored
-  if (storedTasks) {
-    // Iterate through each task and add it to the task list
+  if (storedTasks) {  
     storedTasks.forEach((taskObject) => {
       addTask(taskObject.text);
     });
@@ -82,17 +69,14 @@ window.onload = loadTasks;
 /**
  * Clears only the selected (checked) tasks from the task list and updates localStorage.
  */
-function deleteSelectedTasks() {
-  // Select all checked tasks in the task list
+function deleteSelectedTasks() {  
   let tasks = document.querySelectorAll('#taskList li input[type="checkBox"]:checked');
-
-  // Remove each selected task's list item from the DOM
   tasks.forEach((task) => {
     task.parentElement.remove();
   });
 
-  // Update the remaining tasks in localStorage
   saveTasks();
+
 }
 
 
@@ -100,10 +84,7 @@ function deleteSelectedTasks() {
  * Clears all tasks from the task list and localStorage.
  */
 function deleteAllTasks() {
-  // Remove all task list items from the DOM
   document.getElementById("taskList").innerHTML = "";
-
-  // Remove stored tasks data from localStorage
   localStorage.removeItem("tasks");
 }
 
