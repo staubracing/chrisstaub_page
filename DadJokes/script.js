dd3245ae94df2271159ba0bd4d74d09e7fa16943
StaubRacing <staubracing@gmail.com>
2023-12-23 08:37:15 -0600

    Add Dad Jokes HTML, JavaScript, and CSS files

---
+++ DadJokes/script.js
@@ -0,0 +1,22 @@
+document.getElementById('apiButton').addEventListener('click', function () {
+
+    const existingJoke = document.querySelector('.joke');
+
+
+    if (existingJoke) {
+        existingJoke.remove();
+    }
+
+    axios.get('https://icanhazdadjoke.com/', {
+        headers: {
+            Accept: 'application/json',
+        },
+    })
+        .then(response => {
+            const p = document.createElement('p');
+            p.textContent = response.data.joke;
+            p.className = 'joke'; // Assign the class 'joke' to the paragraph
+            document.body.appendChild(p);
+        })
+        .catch(error => console.error('Error:', error));
+});
