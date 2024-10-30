8d41e672e3a98f11576332d36ba5c185647cf47e
staubracing <staubracing@gmail.com>
2024-10-30 05:36:39 -0500

    update site

--- DadJokes/script.js
+++ DadJokes/script.js
@@ -1,22 +1,21 @@
 document.getElementById('apiButton').addEventListener('click', function () {
+  const existingJoke = document.querySelector('.joke');
 
-    const existingJoke = document.querySelector('.joke');
+  if (existingJoke) {
+    existingJoke.remove();
+  }
 
-
-    if (existingJoke) {
-        existingJoke.remove();
-    }
-
-    axios.get('https://icanhazdadjoke.com/', {
-        headers: {
-            Accept: 'application/json',
-        },
+  axios
+    .get('https://icanhazdadjoke.com/', {
+      headers: {
+        Accept: 'application/json',
+      },
     })
-        .then(response => {
-            const p = document.createElement('p');
-            p.textContent = response.data.joke;
-            p.className = 'joke'; // Assign the class 'joke' to the paragraph
-            document.body.appendChild(p);
-        })
-        .catch(error => console.error('Error:', error));
+    .then((response) => {
+      const p = document.createElement('p');
+      p.textContent = response.data.joke;
+      p.className = 'joke'; // Assign the class 'joke' to the paragraph
+      document.body.appendChild(p);
+    })
+    .catch((error) => console.error('Error:', error));
 });
